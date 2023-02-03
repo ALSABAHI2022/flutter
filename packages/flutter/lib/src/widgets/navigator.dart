0cae829277a1af66485ab332ec1c63583dc16706
ALSABAHI2022 <107499775+ALSABAHI2022@users.noreply.github.com>
2023-02-03 21:45:47 +0300

    Update navigator.dart

--- packages/flutter/lib/src/widgets/navigator.dart
+++ packages/flutter/lib/src/widgets/navigator.dart
@@ -1,6 +1,7 @@
 // Copyright 2014 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file.jkjjj
+
 
 import 'dart:async';
 import 'dart:collection';
